bc83b6a1104ca19209fdf9c6d6ffe848357fadd3
Paul Rogers <progers999@hotmail.co.uk>
2021-02-03 16:20:13 +0000

    Helper added with different ship types

---
+++ src/GameHelpers/shipTypes.js
@@ -0,0 +1,24 @@
+const shipTypes = [
+	{
+		name: 'carrier',
+		length: 5
+	},
+	{
+		name: 'battleship',
+		length: 4
+	},
+	{
+		name: 'submarine',
+		length: 3
+	},
+	{
+		name: 'cruiser',
+		length: 3
+	},
+	{
+		name: 'destroyer',
+		length: 2
+	}
+]
+
+export default shipTypes
