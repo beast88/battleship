8027da8b65bbde74ba6a196530502efa80554499
Paul Rogers <progers999@hotmail.co.uk>
2021-02-04 15:34:42 +0000

    Gameboard checks if ships collide with other ships

--- src/Factories/GameboardFactory.js
+++ src/Factories/GameboardFactory.js
@@ -8,25 +8,59 @@ const gameboard = () => {
 		}
 	}
 
+	const collisionCheck = (loc, length, axis) => {
+		let currentLoc = loc
+		let collision = false
+
+		//Find all the cells in the length of the ship and test if they are ships
+
+		if(axis === 'x') {
+			for(var i = 0; i < length; i++) {
+				if(grid[currentLoc].hasShip === true) {
+					collision = true
+					break;
+				} else if(grid[currentLoc].hasShip === false) {
+					currentLoc++
+				}
+			}
+		} else if(axis === 'y') {
+			for(var j = 0; j < length; j++) {
+				if(grid[currentLoc].hasShip === true) {
+					collision = true
+					break;
+				} else if(grid[currentLoc].hasShip === false) {
+					currentLoc = currentLoc + 8
+				}
+			}
+		}
+
+		return collision
+	}
+
 	return {
 		board: grid,
 
 		placeShip(loc, length, axis) {
 			let currentLoc = loc
 
-			//need a function to test the axis
+			if(collisionCheck(loc, length, axis) === false){
 
-			if(axis === 'x'){
-				for(var i = 0; i < length; i++) {
-					grid[currentLoc].hasShip = true
-					currentLoc++
-				}				
-			} else if(axis === 'y') {
-				for(var j = 0; j < length; j++) {
-					grid[currentLoc].hasShip = true
-					currentLoc = currentLoc + 8
+				if(axis === 'x'){
+					for(var i = 0; i < length; i++) {
+						grid[currentLoc].hasShip = true
+						currentLoc++
+					}				
+				} else if(axis === 'y') {
+					for(var j = 0; j < length; j++) {
+						grid[currentLoc].hasShip = true
+						currentLoc = currentLoc + 8
+					}
 				}
+
+			} else {
+				return
 			}
+
 
 		}
 	}
--- src/__tests__/Gameboard.test.js
+++ src/__tests__/Gameboard.test.js
@@ -2,8 +2,12 @@ import gameboard from '../Factories/GameboardFactory'
 import shipTypes from '../GameHelpers/shipTypes'
 
 describe('Test Gameboard functions', () => {
-	let testGameboard = gameboard();
+	let testGameboard;
 	let destroyer = shipTypes[4]
+
+	beforeEach(() => {
+		testGameboard = gameboard();
+	})
 
 	it('Test random cell to see if it has been generated', () => {
 		expect(testGameboard.board[23]).toEqual({isHit: false, hasShip: false})
@@ -22,5 +26,17 @@ describe('Test Gameboard functions', () => {
 	it('Places ships on the y axis', () => {
 		testGameboard.placeShip(0, destroyer.length, 'y');
 		expect(testGameboard.board[8].hasShip).toEqual(true)
+	})
+
+	it('Should not be able to place a ship if a cell is already a ship', () => {
+		testGameboard.placeShip(0, destroyer.length, 'x');
+		testGameboard.placeShip(0, destroyer.length, 'y');
+		expect(testGameboard.board[8].hasShip).toEqual(false)
+	})
+
+	it('Should not place a ship if the ship will collide with another ship', () => {
+		testGameboard.placeShip(5, destroyer.length, 'y');
+		testGameboard.placeShip(12, destroyer.length, 'x');
+		expect(testGameboard.board[12].hasShip).toEqual(false)
 	})
 })
