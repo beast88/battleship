5e13b98487365f708f47cc0710ab982fcb0970d7
Paul Rogers <progers999@hotmail.co.uk>
2021-02-10 15:17:09 +0000

    Game set up under construction, player board is displayed

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,8 +3,14 @@ import PropTypes from 'prop-types'
 
 const Board = (props) => {
 
+	const cells = props.board.board.map((cell, index) => {
+		const ship = cell.hasShip === true ? {background: "red"} : {}
+		return <div className="cell" key={index} style={ship}></div>
+	})
+
 	return (
 		<div className="player-board">
+			{cells}
 		</div>
 	)
 }
--- src/Components/Init.js
+++ src/Components/Init.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import player from '../Factories/PlayerFactory'
+import shipTypes from '../GameHelpers/shipTypes'
+import Board from './Board'
 
 const Init = () => {
+	const player1 = player()
+
+	const player2 = player()
+
+	console.log(player1, player2, shipTypes)
+
+	
+	const handleClick = () => {
+		player1.board.placeShip(0, shipTypes[4], 'x')
+		console.log(player1.board)
+	}
+
 	return (
 		<div className="set-up">
 			<h1>Battleship</h1>
 
+			<div className="ship-placements">
+
+				<Board board={player1.board}/>
+
+				<div className="consoled">
+					<p>Click and drag your ships onto your board.</p>
+
+					<button onClick={handleClick}> Add Ship </button>
+				</div>
+			</div>
 		</div>
 	)
 }
